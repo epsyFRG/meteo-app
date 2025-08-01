a5c774ab04d6d47f9acfb45e0d2eed343d4534db
Emiliano Massari <158219820+epsyFRG@users.noreply.github.com>
2025-08-01 10:34:33 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import HomePage from "./components/HomePage"
+import WeatherDetail from "./components/WeatherDetail"
+import "./App.css"
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="app-container">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/weather/:city" element={<WeatherDetail />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
