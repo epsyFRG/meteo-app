e112ab5e8e644d3b5df938d665fcd2171fddf4cb
Emiliano Massari <158219820+epsyFRG@users.noreply.github.com>
2025-08-01 10:40:13 +0200

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import React from "react"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import HomePage from "./components/HomePage"
 import WeatherDetail from "./components/WeatherDetail"
+
 import "./App.css"
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="app-container">
+    <Router>
+      <main>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/weather/:city" element={<WeatherDetail />} />
         </Routes>
-      </div>
-    </BrowserRouter>
+      </main>
+    </Router>
   )
 }
 
